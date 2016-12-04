7db4e99e370e5fba61bf3fa909642cb2d5baaf7e
Maksim Pysanets <south_fish@map-2.local>
2016-12-04 15:06:13 +0100

    Add webpack to project

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const webpackConfig = {
+    resolve: {
+        extensions: ['', '.js'],
+        alias: {
+            vendor: 'node_modules'
+        }
+    },
+    entry: [
+        path.resolve(__dirname, './app/main.js')
+    ],
+    assets: {
+        quiet: true,
+        publicPath: '/build/'
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        publicPath: '/build/',
+        filename: '[name].bundle.js',
+        chunkFilename: '[name].bundle.js'
+    },
+    module: {
+        loaders: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: "babel-loader",
+            query: {
+              presets: ['es2015', 'react']
+            }
+          }
+        ]
+    }
+};
+
+module.exports = webpackConfig;
