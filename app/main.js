4f3ebe32649ae9d4385526a911618469af346708
Maksim Pysanets <south_fish@map-2.local>
2016-12-13 19:54:24 +0100

    Formated source code

--- app/main.js
+++ app/main.js
@@ -4,15 +4,15 @@ import {TextField, RaisedButton, Checkbox, AppBar} from 'material-ui';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 const style = {
-  margin: 12,
+  margin: 12
 };
 
 const styles = {
   block: {
-    maxWidth: 250,
+    maxWidth: 250
   },
   checkbox: {
-    marginBottom: 16,
+    marginBottom: 16
   },
 };
 
@@ -20,25 +20,28 @@ class LoginForm extends React.Component {
     render() {
       return (
         <div>
-        <MuiThemeProvider>
-          <div>
-          <TextField
-          hintText="Email Field"
-          floatingLabelText="Email"
-          type="email"
-          /><br />
-          <TextField
-          hintText="Password Field"
-          floatingLabelText="Password"
-          type="password"
-          /><br />
-          <Checkbox
-            label="Remember me"
-            style={styles.checkbox}
-            /><br />
-          <RaisedButton label="login" primary={true} style={style} />
-          </div>
-        </MuiThemeProvider>
+          <MuiThemeProvider>
+            <div>
+              <TextField
+                hintText="Email Field"
+                floatingLabelText="Email"
+                type="email"
+              />
+              <br />
+              <TextField
+                hintText="Password Field"
+                floatingLabelText="Password"
+                type="password"
+              />
+              <br />
+              <Checkbox
+                label="Remember me"
+                style={styles.checkbox}
+              />
+              <br />
+              <RaisedButton label="login" primary={true} style={style} />
+            </div>
+          </MuiThemeProvider>
         </div>
       );
     }
@@ -52,13 +55,12 @@ ReactDOM.render(
 class Menu extends React.Component {
   render () {
     return (
-
         <MuiThemeProvider>
           <AppBar
             title="Title"
             iconClassNameRight="muidocs-icon-navigation-expand-more"
           />
-        </MuiThemeProvider >
+        </MuiThemeProvider>
       );
   }
 }
