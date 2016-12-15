2203d9c5408a73977beaf54783e3174b0d6318e4
Maksim Pysanets <m.pysanets@hotmail.com>
2016-12-15 16:03:22 +0100

    moved injectTapEventPlugin initialization to app level

--- app/components/App/index.js
+++ app/components/App/index.js
@@ -3,10 +3,8 @@ import Nav from '../Nav';
 import LoginForm from '../LoginForm';
 import Footer from '../Footer';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 
 const App = () => {
-    injectTapEventPlugin();
     return (
         <MuiThemeProvider>
             <div>
