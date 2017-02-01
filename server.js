fd2369eca7bcb130f6616f01172a30422f437b76
Maksim Pysanets <m.pysanets@hotmail.com>
2017-02-01 21:01:59 +0100

    webpack hapi plugin

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const Hapi = require('hapi');
 const Inert = require('inert');
+const dotenv = require('dotenv');
+
+dotenv.load();
 
 const PORT = process.env.PORT || 5000;
 
@@ -9,6 +12,22 @@ server.connection({ port: PORT });
 
 // Hapi Plugins
 const hapiPlugins = [Inert];
+
+if (process.env.NODE_ENV === 'development') {
+    const Webpack = require('webpack');
+    const WebpackPlugin = require('hapi-webpack-plugin');
+
+    const devConfig = require('./webpack.config.development');
+    const compiler = new Webpack(devConfig);
+
+    hapiPlugins.push({
+        register: WebpackPlugin,
+        options: {
+            compiler,
+            assets: devConfig.assets
+        }
+    });
+}
 
 server.register(hapiPlugins, (error) => {
     if (error) throw error;
@@ -26,5 +45,6 @@ server.register(hapiPlugins, (error) => {
 
     return server.start((err) => {
         if (err) throw err;
+        console.log('Server running at:', server.info.uri);
     });
-})
+});
